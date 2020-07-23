706eee3babbefa2d9c2cc396ff3942b12997a3fb
unknown <roberto27365@outlook.com>
2020-07-23 10:57:35 -0400

    finished wack-a-mole

--- index.js
+++ index.js
@@ -1,6 +1,31 @@
-
-
 let score = 0;
 var scoreDisplay = document.getElementById('score');
 
-console.log(scoreDisplay);
+var holes = document.getElementsByClassName('hole');
+
+setInterval(function(){
+    var randomHoles = Math.floor(Math.random() * holes.length);
+
+    holes[randomHoles].classList.toggle('mole');
+},1000);
+
+var gameArea = document.getElementById
+("whack-a-mole");
+
+gameArea.addEventListener('click',function(clickEvent){
+    if(clickEvent.target.matches('.mole')){
+
+        clickEvent.target.classList.remove('mole')
+
+        score++
+
+        scoreDisplay.innerText = score;
+    }
+});
+
+
+
+
+
+
+
